fa61905a133791e7a3062a2b1766442f9ed2c5b7
miron.bogdan <alexbogdanmiron@gmail.com>
2026-03-16 23:39:27 +0200

    Added DTOs

---
+++ BankApp/BankApp.Models/DTOs/Dashboard/DashboardResponse.cs
@@ -0,0 +1,10 @@
+﻿using BankApp.Models.Entities;
+namespace BankApp.Models.DTOs.Dashboard
+{
+    public class DashboardResponse
+    {
+        public List<Card> Cards { get; set; } = new();
+        public List<Transaction> RecentTransactions { get; set; } = new();
+        public int UnreadNotificationCount { get; set; }
+    }
+}
---
+++ BankApp/BankApp.Models/DTOs/Profile/UpdateProfileRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace BankApp.Models.DTOs.Profile
+{
+    public class UpdateProfileRequest
+    {
+        public string? PhoneNumber { get; set; }
+        public string? Address { get; set; }
+    }
+}
