caf2cbf521874161d96169444135e78c44938d10
muresanianis450 <ianis.constantin.muresan@stud.ubbcluj.ro>
2026-03-18 20:25:12 +0200

    Create IJWTService.cs

---
+++ BankApp/BankApp.Server/Services/Infrastructure/Interfaces/IJWTService.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Claims;
+
+namespace BankApp.Server.Services.Infrastructure;
+
+public interface IJWTService
+{
+    string GenerateToken(int userId);
+    ClaimsPrincipal? ValidateToken(string token);
+    int? ExtractUserId(string token);
+}
