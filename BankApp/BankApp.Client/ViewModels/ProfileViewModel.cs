b032edfc52417ad6384d1b14cc0e28810f025b03
Bogdan Mocrei <mocrei.bogdhan@gmail.com>
2026-03-18 23:16:17 +0200

    ProfileViewModel skeleton

--- BankApp/BankApp.Client/ViewModels/ProfileViewModel.cs
+++ BankApp/BankApp.Client/ViewModels/ProfileViewModel.cs
@@ -1,9 +1,58 @@
-﻿using BankApp.Client.ViewModels.Base;
+﻿using BankApp.Client.Utilities;
+using BankApp.Client.ViewModels.Base;
+using BankApp.Models.Entities;
 using BankApp.Models.Enums;
+using System.Collections.Generic;
+using System;
+
 namespace BankApp.Client.ViewModels
 {
     public class ProfileViewModel : BaseViewModel
     {
-        // TODO
-    }
+        public Observable<ProfileState> State { get; private set; }
+        public Observable<User> CurrentUser { get; private set; }
+        public Observable<List<OAuthLink>> OAuthLinks { get; private set; }
+        public Observable<List<Session>> ActiveSessions { get; private set; }
+        public Observable<List<NotificationPreference>> NotificationPreferences { get; private set; }
+
+        public bool UpdatePersonalInfo(string phone, string address, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ChangePassword(string currentPassword, string newPassword)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EnableTwoFactor(TwoFactorMethod method)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DisableTwoFactor(TwoFactorMethod method)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool LinkOAuth(string provider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UnlinkOAuth(string provider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateNotificationPreferences(List<NotificationPreference> preferences)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    } 
 }
