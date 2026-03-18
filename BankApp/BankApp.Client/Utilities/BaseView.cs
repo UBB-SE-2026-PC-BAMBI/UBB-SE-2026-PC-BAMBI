f389f1b5fba9ad98b60a63b5bc2a08535fa14445
Bogdan Mocrei <mocrei.bogdhan@gmail.com>
2026-03-18 21:07:23 +0200

    Done View Utils
    - Implemented Observable<T> class
    - Done interfaces INotifyPropertyChanged, BaseViewModel, Observer<T>
    - Implemented abstract class BaseView<T>

---
+++ BankApp/BankApp.Client/Utilities/BaseView.cs
@@ -0,0 +1,16 @@
+﻿namespace BankApp.Client.Utilities
+{
+    public abstract class BaseView<T>
+    {
+        public void Update(T value)
+        {
+            OnStateChanged(value);
+        }
+
+        public abstract void BindViewModel(BaseViewModel viewModel);
+        public abstract void OnStateChanged(T state);
+        public abstract void ShowError(string msg);
+        public abstract void ShowLoading();
+        public abstract void HideLoading();
+    }
+}
