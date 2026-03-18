0a0163bde5662c3daff84f79805360b54b0edd19
Bogdan Mocrei <mocrei.bogdhan@gmail.com>
2026-03-18 21:32:14 +0200

    Deprecated BaseViewModel
    Deprecated old BaseViewModel

---
+++ BankApp/BankApp.Client/ViewModels/Base/BaseViewModel_old.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel;
+using Windows.Foundation.Metadata;
+namespace BankApp.Client.ViewModels.Base
+{
+    // Deprecated, at least according to the UML diagram. New version can be found in Utilities
+    [Obsolete]
+    public abstract class BaseViewModel_old : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
