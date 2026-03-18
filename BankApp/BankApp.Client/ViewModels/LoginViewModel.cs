bca54a001e15521d655e3ca4ef6f14ebefb73486
Bogdan Mocrei <mocrei.bogdhan@gmail.com>
2026-03-18 22:34:54 +0200

    LoginViewModel skeleton
    Skeleton for LoginViewModel

--- BankApp/BankApp.Client/ViewModels/LoginViewModel.cs
+++ BankApp/BankApp.Client/ViewModels/LoginViewModel.cs
@@ -1,9 +1,44 @@
 ﻿using BankApp.Client.ViewModels.Base;
 using BankApp.Models.Enums;
+using BankApp.Client.Utilities;
+using BankApp.Models.DTOs.Auth;
+using System;
 namespace BankApp.Client.ViewModels
 {
     public class LoginViewModel : BaseViewModel
     {
-        // TODO
+        public Observable<LoginState> State { get; private set; }
+
+        public LoginViewModel()
+        {
+            State = new Observable<LoginState>(LoginState.Idle);
+            // maybe more
+        }
+
+        public void Login(string email, string password)
+        {
+            // Create a LoginRequest DTO and send it
+            throw new NotImplementedException();
+        }
+
+        public void OAuthLogin(string email, string provider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool OpenRegister()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool OpenForgotPassword()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Dispose()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
