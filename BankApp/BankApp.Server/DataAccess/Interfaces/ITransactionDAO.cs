2eb4c77cbaa9f0cf8b615ea7074a9877fb1f88b9
Patricia Mateica <patriciamateica33@gmail.com>
2026-03-19 20:24:39 +0200

    Implemented TransactionDAO - implemented FindRecentByAccountId - added MapToEntityMethod - fixed ITransactionDAO again because i overlooked changing the attributed from userId to accountId

--- BankApp/BankApp.Server/DataAccess/Interfaces/ITransactionDAO.cs
+++ BankApp/BankApp.Server/DataAccess/Interfaces/ITransactionDAO.cs
@@ -3,6 +3,6 @@ namespace BankApp.Server.DataAccess.Interfaces
 {
     public interface ITransactionDAO
     {
-        List<Transaction> FindRecentByAccountId(int userId, int limit = 10);
+        List<Transaction> FindRecentByAccountId(int accountId, int limit = 10);
     }
 }
