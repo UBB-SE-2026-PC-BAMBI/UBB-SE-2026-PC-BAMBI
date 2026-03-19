735cfb2eb44294d18d173ed0f95dc48fde5a3f6e
Patricia Mateica <patriciamateica33@gmail.com>
2026-03-19 20:20:58 +0200

    Reapply "Changed FindRecentByUserId method to FindRecentByAccountId since there's no userId foreign key in the Transaction Table"
    This reverts commit ec2732d4581ccea6ffe9cfd490f5fcdd5e53db11.

--- BankApp/BankApp.Server/DataAccess/Interfaces/ITransactionDAO.cs
+++ BankApp/BankApp.Server/DataAccess/Interfaces/ITransactionDAO.cs
@@ -3,6 +3,6 @@ namespace BankApp.Server.DataAccess.Interfaces
 {
     public interface ITransactionDAO
     {
-        List<Transaction> FindRecentByUserId(int userId, int limit = 10);
+        List<Transaction> FindRecentByAccountId(int userId, int limit = 10);
     }
 }
