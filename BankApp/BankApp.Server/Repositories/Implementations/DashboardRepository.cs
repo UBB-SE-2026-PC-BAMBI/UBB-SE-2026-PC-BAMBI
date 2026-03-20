05f3ca885d5aa0bfb9752b46c179e5b8b635d30c
Andrei-Ilie Mateian <mailandrei2014@gmail.com>
2026-03-20 10:46:52 +0200

    Repo with abstract classes NOT TESTED
    The DAOs are not yet merged, so this is a first try, not tested

---
+++ BankApp/BankApp.Server/Repositories/Implementations/DashboardRepository.cs
@@ -0,0 +1,37 @@
+using BankApp.Models.Entities;
+
+namespace BankApp.Server.Repositories.Implementations
+{
+	public class DashboardRepository : IDashboardRepository
+	{
+		private readonly AccountDAO accountDAO;
+		private readonly CardDAO cardDAO;
+		private readonly TransactionDAO transactionDAO;
+		private readonly NotificationDAO notificationDAO;
+
+		public DashboardRepository(AccountDAO accountDAO, CardDAO cardDAO, TransactionDAO transactionDAO, NotificationDAO notificationDAO)
+		{
+			this.accountDAO = accountDAO;
+			this.cardDAO = cardDAO;
+			this.transactionDAO = transactionDAO;
+			this.notificationDAO = notificationDAO;
+		}
+
+		public List<Account> GetAccountsByUser(int userId)
+		{
+			return this.accountDAO.GetAccountsByUser(userId);
+		}
+		public List<Card> GetCardsByUser(int userId)
+		{
+			return this.cardDAO.GetCardsByUser(userId);
+		}
+		public List<Transaction> GetRecentTransactions(int userId, int limit = 10)
+		{
+			return this.transactionDAO.GetRecentTransactions(userId, limit);
+		}
+		public int GetUnreadNotificationCount(int userId)
+		{
+			return this.notificationDAO.GetUnreadCountByUser(userId);
+		}
+	}
+}
