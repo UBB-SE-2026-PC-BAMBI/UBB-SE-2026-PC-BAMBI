b39b74bf93b653274f552a8afe6717f5ee6fdc7e
Patricia Mateica <patriciamateica33@gmail.com>
2026-03-20 22:26:49 +0200

    Fixed the private fields in the repository dashboard so all private fields go by the same convention

--- BankApp/BankApp.Server/Repositories/Implementations/DashboardRepository.cs
+++ BankApp/BankApp.Server/Repositories/Implementations/DashboardRepository.cs
@@ -6,34 +6,34 @@ namespace BankApp.Server.Repositories.Implementations
 {
 	public class DashboardRepository : IDashboardRepository
 	{
-		private readonly IAccountDAO accountDAO;
-		private readonly ICardDAO cardDAO;
-		private readonly ITransactionDAO transactionDAO;
-		private readonly INotificationDAO notificationDAO;
+		private readonly IAccountDAO _accountDAO;
+		private readonly ICardDAO _cardDAO;
+		private readonly ITransactionDAO _transactionDAO;
+		private readonly INotificationDAO _notificationDAO;
 
 		public DashboardRepository(IAccountDAO accountDAO, ICardDAO cardDAO, ITransactionDAO transactionDAO, INotificationDAO notificationDAO)
 		{
-			this.accountDAO = accountDAO;
-			this.cardDAO = cardDAO;
-			this.transactionDAO = transactionDAO;
-			this.notificationDAO = notificationDAO;
+			_accountDAO = accountDAO;
+			_cardDAO = cardDAO;
+			_transactionDAO = transactionDAO;
+			_notificationDAO = notificationDAO;
 		}
 
 		public List<Account> GetAccountsByUser(int userId)
 		{
-			return this.accountDAO.FindByUserId(userId);
+			return _accountDAO.FindByUserId(userId);
 		}
 		public List<Card> GetCardsByUser(int userId)
 		{
-			return this.cardDAO.FindByUserId(userId);
+			return _cardDAO.FindByUserId(userId);
 		}
 		public List<Transaction> GetRecentTransactions(int accountId, int limit = 10)
 		{
-			return this.transactionDAO.FindRecentByAccountId(accountId, limit);
+			return _transactionDAO.FindRecentByAccountId(accountId, limit);
 		}
 		public int GetUnreadNotificationCount(int userId)
 		{
-			return this.notificationDAO.CountUnreadByUserId(userId);
+			return _notificationDAO.CountUnreadByUserId(userId);
 		}
 	}
 }
