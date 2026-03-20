6aea0768a93cdcddcacdde84996c66b37adb41c1
Cosmin-Georgian Mocanu <cosmin27.mocanu@gmail.com>
2026-03-20 18:39:42 +0200

    Implementing the NotificationPreferenceDAO
    Finished and tested

---
+++ BankApp/BankApp.Server/DataAccess/NotificationPrefenceDAO.cs
@@ -0,0 +1,81 @@
+﻿using BankApp.Models.Entities;
+using BankApp.Server.DataAccess.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp.Server.DataAccess
+{
+     internal class NotificationPreferenceDAO : INotificationPreferenceDAO
+    {
+
+        private AppDbContext _appDbContext;
+
+        public NotificationPreferenceDAO(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public List<NotificationPreference> FindByUserId(int userId)
+        {
+            List<NotificationPreference> result = new List<NotificationPreference>();
+            string selectQuery = @"SELECT * FROM NotificationPreference WHERE userId = @p0";
+
+            using IDataReader data = this._appDbContext.ExecuteQuery(selectQuery, [userId]);
+
+
+            while (data.Read())
+            {
+                NotificationPreference notificationPreference = new NotificationPreference
+                {
+                    Id = Convert.ToInt32(data["Id"]),
+                    UserId = Convert.ToInt32(data["UserId"]),
+                    Category = Convert.ToString(data["Category"]),
+                    PushEnabled = Convert.ToBoolean(data["PushEnabled"]),
+                    EmailEnabled = Convert.ToBoolean(data["EmailEnabled"]),
+                    SmsEnabled = Convert.ToBoolean(data["SmsEnabled"]),
+                    MinAmountThreshold = Convert.ToDecimal(data["MinAmountThreshold"])
+                };
+
+                result.Add(notificationPreference);
+
+            }
+
+            return result;
+        }
+        public bool Update(int userId, List<NotificationPreference> prefs)
+        {
+            try
+            {
+                string deleteQuery = @"DELETE FROM NotificationPreference WHERE userId = @p0";
+                this._appDbContext.ExecuteNonQuery(deleteQuery, [userId]);
+
+                string insertQuery = @"INSERT INTO NotificationPreference (UserId, Category, PushEnabled, EmailEnabled, SmsEnabled, MinAmountThreshold)
+                                        VALUES
+                                    (@p0, @p1, @p2, @p3, @p4, @p5);
+                                ";
+
+                foreach (NotificationPreference preference in prefs)
+                {
+                    this._appDbContext.ExecuteNonQuery(insertQuery, [
+                            preference.UserId,
+                        preference.Category,
+                        preference.PushEnabled,
+                        preference.EmailEnabled,
+                        preference.SmsEnabled,
+                        preference.MinAmountThreshold
+                        ]);
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
