185ce3649e5bfdf8ef3d14416345dcd45b84b206
Bogdan Mocrei <mocrei.bogdhan@gmail.com>
2026-03-21 18:59:22 +0200

    Changed and implemented ProfileService.UpdatePersonalInfo

--- BankApp/BankApp.Server/Services/Implementations/ProfileService.cs
+++ BankApp/BankApp.Server/Services/Implementations/ProfileService.cs
@@ -1,8 +1,10 @@
-﻿using BankApp.Models.Entities;
+﻿using BankApp.Models.DTOs.Profile;
+using BankApp.Models.Entities;
 using BankApp.Models.Enums;
 using BankApp.Server.Repositories;
 using BankApp.Server.Repositories.Interfaces;
 using BankApp.Server.Services.Interfaces;
+using BankApp.Server.Utilities;
 
 namespace BankApp.Server.Services.Implementations
 {
@@ -20,48 +22,79 @@ namespace BankApp.Server.Services.Implementations
             return _userRepository.FindById(userId);
         }
 
-        public bool UpdatePersonalInfo(int userId, string? phone, string? address)
+        public UpdateProfileResponse UpdatePersonalInfo(UpdateProfileRequest request)
         {
-            User? user = _userRepository.FindById(userId);
+            User? user = _userRepository.FindById(request.UserId);
 
             if (user == null)
             {
-                return false;
+                return new UpdateProfileResponse(false, "User not found.");
             }
 
-            if (phone != null) {
+            // Check and update phone number
+            if (request.PhoneNumber != null)
+            {
+                if (!ValidationUtil.IsValidPhoneNumber(request.PhoneNumber))
+                {
+                    return new UpdateProfileResponse(false, "Invalid phone number.");
+                }
+
+                user.PhoneNumber = request.PhoneNumber;
+            }
+
+            // Check and update address
+            if (request.Address != null)
+            {
+                user.Address = request.Address;
+            }
+
+            // Update the user in the repo
+            if (_userRepository.UpdateUser(user) == false)
+            {
+                return new UpdateProfileResponse(false, "Could not update user.");
+            }
+
+            return new UpdateProfileResponse(true, "User profile updated successfully.");
         }
 
         public bool ChangePassword(int userId, string currentPassword, string newPassword)
         {
+            throw new NotImplementedException();
         }
 
         public bool Enable2FA(int userId, TwoFactorMethod method)
         {
+            throw new NotImplementedException();
         }
 
         public bool Disable2FA(int userId, TwoFactorMethod method)
         {
+            throw new NotImplementedException();
         }
 
         public List<OAuthLink> GetOAuthLinks(int userId)
         {
+            throw new NotImplementedException();
         }
 
         public bool LinkOAuth(int userId, string provider)
         {
+            throw new NotImplementedException();
         }
 
         public bool UnlinkOAuth(int userId, string provider)
         {
+            throw new NotImplementedException();
         }
 
         public List<NotificationPreference> GetNotificationPreferences(int userId)
         {
+            throw new NotImplementedException();
         }
 
         public bool UpdateNotificationPreferences(int userId, List<NotificationPreference> prefs)
         {
+            throw new NotImplementedException();
         }
     }
 }
--- BankApp/BankApp.Server/Services/Interfaces/IProfileService.cs
+++ BankApp/BankApp.Server/Services/Interfaces/IProfileService.cs
@@ -1,11 +1,12 @@
-﻿using BankApp.Models.Entities;
+﻿using BankApp.Models.DTOs.Profile;
+using BankApp.Models.Entities;
 using BankApp.Models.Enums;
 namespace BankApp.Server.Services.Interfaces
 {
     public interface IProfileService
     {
         User? GetUserById(int userId);
-        bool UpdatePersonalInfo(int userId, string? phone, string? address);
+        UpdateProfileResponse UpdatePersonalInfo(UpdateProfileRequest request);
         bool ChangePassword(int userId, string currentPassword, string newPassword);
         bool Enable2FA(int userId, TwoFactorMethod method);
         bool Disable2FA(int userId, TwoFactorMethod method);
