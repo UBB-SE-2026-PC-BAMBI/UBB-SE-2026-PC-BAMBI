3dcc56bcdcfee64acdae443eb8c3b66c9d9cc252
Bogdan Mocrei <mocrei.bogdhan@gmail.com>
2026-03-21 18:20:59 +0200

    Dummy ProfileService + ValidationUtil update
    - Dummy raw version of ProfileService, to be implemented
    - Dummy UserRepository, TO BE OVERRIDEN by Ianis' work
    - IsValidPhoneNumber method in ValidationUtil

---
+++ BankApp/BankApp.Server/Repositories/Implementations/UserRepository.cs
@@ -0,0 +1,37 @@
+﻿using BankApp.Models.Entities;
+using BankApp.Server.Repositories.Interfaces;
+
+namespace BankApp.Server.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+
+        // OVERRIDE THIS DUMMY IMPLEMENTATION WITH THE REAL IMPLEMENTATION
+        public User? FindById(int userId) =>
+            throw new NotImplementedException();
+
+        public bool UpdateUser(User user) =>
+            throw new NotImplementedException();
+
+        public List<Session> GetActiveSessions(int userId) =>
+            throw new NotImplementedException();
+
+        public void RevokeSession(int sessionId) =>
+            throw new NotImplementedException();
+
+        public List<OAuthLink> GetLinkedProviders(int userId) =>
+            throw new NotImplementedException();
+
+        public bool SaveOAuthLink(int userId, string provider, string providerUserId, string? email) =>
+            throw new NotImplementedException();
+
+        public void DeleteOAuthLink(int linkId) =>
+            throw new NotImplementedException();
+
+        public List<NotificationPreference> GetNotificationPreferences(int userId) =>
+            throw new NotImplementedException();
+
+        public bool UpdateNotificationPreferences(int userId, List<NotificationPreference> prefs) =>
+            throw new NotImplementedException();
+    }
+}
---
+++ BankApp/BankApp.Server/Services/Implementations/ProfileService.cs
@@ -0,0 +1,67 @@
+﻿using BankApp.Models.Entities;
+using BankApp.Models.Enums;
+using BankApp.Server.Repositories;
+using BankApp.Server.Repositories.Interfaces;
+using BankApp.Server.Services.Interfaces;
+
+namespace BankApp.Server.Services.Implementations
+{
+    public class ProfileService : IProfileService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public ProfileService()
+        {
+            _userRepository = new UserRepository();
+        }
+
+        public User? GetUserById(int userId)
+        {
+            return _userRepository.FindById(userId);
+        }
+
+        public bool UpdatePersonalInfo(int userId, string? phone, string? address)
+        {
+            User? user = _userRepository.FindById(userId);
+
+            if (user == null)
+            {
+                return false;
+            }
+
+            if (phone != null) {
+        }
+
+        public bool ChangePassword(int userId, string currentPassword, string newPassword)
+        {
+        }
+
+        public bool Enable2FA(int userId, TwoFactorMethod method)
+        {
+        }
+
+        public bool Disable2FA(int userId, TwoFactorMethod method)
+        {
+        }
+
+        public List<OAuthLink> GetOAuthLinks(int userId)
+        {
+        }
+
+        public bool LinkOAuth(int userId, string provider)
+        {
+        }
+
+        public bool UnlinkOAuth(int userId, string provider)
+        {
+        }
+
+        public List<NotificationPreference> GetNotificationPreferences(int userId)
+        {
+        }
+
+        public bool UpdateNotificationPreferences(int userId, List<NotificationPreference> prefs)
+        {
+        }
+    }
+}
