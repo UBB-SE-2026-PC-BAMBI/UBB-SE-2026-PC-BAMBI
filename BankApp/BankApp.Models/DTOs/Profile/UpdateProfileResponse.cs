d81ed89c50c1a1e1c5c14374bb5cbda3fed6f87a
Bogdan Mocrei <mocrei.bogdhan@gmail.com>
2026-03-21 18:46:17 +0200

    Created UpdateProfileResponse
    - New DTO for Profile

---
+++ BankApp/BankApp.Models/DTOs/Profile/UpdateProfileResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp.Models.DTOs.Profile
+{
+    public class UpdateProfileResponse
+    {
+        public bool Success { get; set; }
+        public string? Message { get; set; }
+
+        public UpdateProfileResponse(bool success, string? message)
+        {
+            Success = success;
+            Message = message;
+        }
+    }
+}
