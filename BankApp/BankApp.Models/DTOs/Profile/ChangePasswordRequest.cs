7c4b29fc33ed14a2479d397db4255bff8aeb3ae1
Bogdan Mocrei <mocrei.bogdhan@gmail.com>
2026-03-21 19:00:38 +0200

    Updated ChangePasswordRequest
    - Added UserId property

--- BankApp/BankApp.Models/DTOs/Profile/ChangePasswordRequest.cs
+++ BankApp/BankApp.Models/DTOs/Profile/ChangePasswordRequest.cs
@@ -2,6 +2,7 @@
 {
     public class ChangePasswordRequest
     {
+        public int UserId { get; set; }
         public string CurrentPassword { get; set; } = string.Empty;
         public string NewPassword { get; set; } = string.Empty;
     }
