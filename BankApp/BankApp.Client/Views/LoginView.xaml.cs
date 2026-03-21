e56e6f8ea66803c012acd09e1f8b1c33dade408c
Andrei-Ilie Mateian <mailandrei2014@gmail.com>
2026-03-21 15:28:21 +0200

    emoji still up, will be tested further in the future
    JVNAJVASVNAVASPVSNAVA

--- BankApp/BankApp.Client/Views/LoginView.xaml.cs
+++ BankApp/BankApp.Client/Views/LoginView.xaml.cs
@@ -40,7 +40,7 @@ namespace BankApp.Client.Views
                         break;
 
                     case LoginState.Success:
-                        App.NavigationService.NavigateTo<LoginView>(); // TODO: replace with DashboardView
+                        App.NavigationService.NavigateTo<DashboardView>(); // TODO: replace with DashboardView -> done
                         break;
 
                     case LoginState.Require2FA:
