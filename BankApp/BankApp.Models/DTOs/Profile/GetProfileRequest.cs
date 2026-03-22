9a1c036dd85023db78ae222227e41f15368e56ea
Bogdan Mocrei <mocrei.bogdhan@gmail.com>
2026-03-22 19:52:16 +0200

    Deprecated GetProfileRequest
    - Currently deprecated as the GetProfile endpoint incorporates its only property, the userId, in the URL
    - Not deleted yet as the endpoint request might be changed in the future to encapsulate more information

--- BankApp/BankApp.Models/DTOs/Profile/GetProfileRequest.cs
+++ BankApp/BankApp.Models/DTOs/Profile/GetProfileRequest.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 
 namespace BankApp.Models.DTOs.Profile
 {
+    // DEPRECATED:
+    // Currently deprecated as the GetProfile endpoint incorporates its only property, the userId, in the URL
+    // Not deleted yet as the endpoint request might be changed in the future to encapsulate more information
+    [Obsolete]
     public class GetProfileRequest
     {
         public int UserId { get; set; }
