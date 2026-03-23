c745bacd76ec728bea03d18dcbb97159a53d011d
Bogdan Mocrei <mocrei.bogdhan@gmail.com>
2026-03-23 20:54:42 +0200

    Updated ChangePasswordRequest
    - Added constructor

--- BankApp/BankApp.Models/DTOs/Profile/ChangePasswordRequest.cs
+++ BankApp/BankApp.Models/DTOs/Profile/ChangePasswordRequest.cs
@@ -5,5 +5,14 @@
         public int UserId { get; set; }
         public string CurrentPassword { get; set; } = string.Empty;
         public string NewPassword { get; set; } = string.Empty;
+
+        public ChangePasswordRequest() { }
+        
+        public ChangePasswordRequest(int userId, string currentPassword, string newPassword)
+        {
+            UserId = userId;
+            CurrentPassword = currentPassword;
+            NewPassword = newPassword;
+        }   
     }
 }
