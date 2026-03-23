5e69db8be878cb9f8d6b6200384a58037cfde73b
Bogdan Mocrei <mocrei.bogdhan@gmail.com>
2026-03-23 20:03:35 +0200

    Updated UpdateProfileRequest
    - Constructor added

--- BankApp/BankApp.Models/DTOs/Profile/UpdateProfileRequest.cs
+++ BankApp/BankApp.Models/DTOs/Profile/UpdateProfileRequest.cs
@@ -5,5 +5,14 @@
         public int UserId { get; set; }
         public string? PhoneNumber { get; set; }
         public string? Address { get; set; }
+
+        public UpdateProfileRequest() { }
+
+        public UpdateProfileRequest(int userId, string? phoneNumber, string? address)
+        {
+            UserId = userId;
+            PhoneNumber = phoneNumber;
+            Address = address;
+        }
     }
 }
