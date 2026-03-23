fa0ef72b72a5391f7b0440f76ceb370187b5f578
muresanianis450 <ianis.constantin.muresan@stud.ubbcluj.ro>
2026-03-23 20:09:55 +0200

    Update NotificationPreference.cs

--- BankApp/BankApp.Models/Entities/NotificationPreference.cs
+++ BankApp/BankApp.Models/Entities/NotificationPreference.cs
@@ -1,10 +1,13 @@
-﻿namespace BankApp.Models.Entities
+﻿using BankApp.Models.Enums;
+
+namespace BankApp.Models.Entities
 {
     public class NotificationPreference
     {
         public int Id { get; set; }
         public int UserId { get; set; }
-        public string Category { get; set; } = string.Empty;
+        public NotificationType Category { get; set; }
+        //public string Category { get; set; } = string.Empty;
         public bool PushEnabled { get; set; } = true;
         public bool EmailEnabled { get; set; } = true;
         public bool SmsEnabled { get; set; }
