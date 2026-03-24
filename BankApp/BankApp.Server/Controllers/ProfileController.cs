02c6f4144a404bc1c2aca248992b28a82383207b
Bogdan Mocrei <mocrei.bogdhan@gmail.com>
2026-03-25 00:33:10 +0200

    Added VerifyPassword endpoint to ProfileController

--- BankApp/BankApp.Server/Controllers/ProfileController.cs
+++ BankApp/BankApp.Server/Controllers/ProfileController.cs
@@ -97,11 +97,27 @@ namespace BankApp.Server.Controllers
         }
 
         // PUT: api/profile/{userId}/notifications/preferences
-        // Serializes: -
+        // Serializes: bool
         [HttpPut("{userId}/notifications/preferences")]
         public IActionResult UpdateNotificationPreferences(int userId, [FromBody] List<NotificationPreference> prefs)
         {
             bool success = _profileService.UpdateNotificationPreferences(userId, prefs);
+
+            if (!success)
+            {
+                return BadRequest(false);
+            }
+
+            return Ok(true);
+        }
+
+        // POST: api/profile/{userId}/verify-password
+        // Serializes: bool
+        [HttpPost("{userId}/verify-password")]
+        public IActionResult VerifyPassword(int userId, [FromBody] string password)
+        {
+            bool success = _profileService.VerifyPassword(userId, password);
+
             if (!success)
             {
                 return BadRequest(false);
